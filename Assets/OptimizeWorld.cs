b05ac5a77dca953efe4990ffdd0d9d4578f294a2
KirikD <fx3kd@yandex.ru>
2023-07-04 03:52:33 +0500

    dtr

--- Assets/OptimizeWorld.cs
+++ Assets/OptimizeWorld.cs
@@ -1,7 +1,7 @@
 using EasyRoads3Dv3;
 using System.Collections.Generic;
 using Unity.Mathematics;
-using UnityEditor;
+//using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class OptimizeWorld : MonoBehaviour
@@ -13,7 +13,7 @@ public class OptimizeWorld : MonoBehaviour
 
         foreach (GameObject go in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
         {
-            if (!EditorUtility.IsPersistent(go.transform.root.gameObject) && !(go.hideFlags == HideFlags.NotEditable || go.hideFlags == HideFlags.HideAndDontSave))
+           // if (!EditorUtility.IsPersistent(go.transform.root.gameObject) && !(go.hideFlags == HideFlags.NotEditable || go.hideFlags == HideFlags.HideAndDontSave))
                 objectsInScene.Add(go);
         }
 
@@ -47,23 +47,27 @@ public class OptimizeWorld : MonoBehaviour
         {
             if (allObjects[i].GetComponent<MeshRenderer>() != null && allObjects[i].gameObject.tag == "WorldObjs") //
             {
+                if (allObjects[i].name == "surface") //  surface
+                { allObjects[i].transform.Translate(new Vector3(0, -0.025f, 0), Space.World);  }
                 Opt.Add(allObjects[i].GetComponent<MeshRenderer>());
 
             }
             if (allObjects[i].GetComponent<ERModularRoad>() != null)
                 Destroy(allObjects[i].GetComponent<ERModularRoad>());
         }
-        coords = new Vector3[Opt.Count];
+        coords = new Vector3[Opt.Count]; TransCoords = new Vector3[Opt.Count];
         for (int ii = 0; ii < Opt.Count; ii++)
         {
             Bounds Bou = Opt[ii].GetComponent<MeshFilter>().mesh.bounds;
             coords[ii] = Bou.center;
+            TransCoords[ii] = Opt[ii].transform.TransformPoint(coords[ii]);
             float dist = math.distance(Cam.position, Opt[ii].transform.TransformPoint(coords[ii]));
             if (dist > GlobalDistance)
             { Opt[ii].enabled = false; }
             else
                 Opt[ii].enabled = true;
 
+           
             /*GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             sphere.transform.position = Opt[ii].transform.TransformPoint(coords[ii]);
             sphere.name = Opt[ii].name;
@@ -86,33 +90,48 @@ public class OptimizeWorld : MonoBehaviour
     }
     void UpdHid()
     {
+        
         for (int ii = 0; ii < Opt.Count; ii++)
         {
-            float dist = math.distance(Cam.position, Opt[ii].transform.TransformPoint(coords[ii]));
+            
+            float dist = math.distance(Cam.position, Opt[ii].transform.TransformPoint(coords[ii])); //float dist = math.distance(Cam.position, TransCoords[ii]); 
             if (dist > GlobalDistance)
             { Opt[ii].enabled = false; }
             else
                 Opt[ii].enabled = true;
+            
         }
+        
     }
     public List<MeshRenderer> Opt = new List<MeshRenderer>();
-    public Vector3[] coords;
+    public Vector3[] coords; public Vector3[] TransCoords;
     int adder = 0;
+    public AudioSource Beep; 
     void FixedUpdate()
-    { if (enablerUpdate) { 
-        float dist = math.distance(Cam.position, Opt[adder].transform.TransformPoint(coords[adder]));
-        if (dist > GlobalDistance)
-        { Opt[adder].enabled = false; }
-        else
-            Opt[adder].enabled = true;
+    { if (enablerUpdate) {
+            
+            float dist = math.distance(Cam.position, Opt[adder].transform.TransformPoint(coords[adder]));
+            if (dist > GlobalDistance)
+            { Opt[adder].enabled = false;  }
+            else
+            { Opt[adder].enabled = true;  }
         adder++;
         if (adder > Opt.Count - 1)
             adder = 0;
 
+
+        } 
+}
+    void Update()
+    {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             UpdHid();
         }
-    } 
-}
+        Beep.volume = 0.0F;
+        if (Input.GetKey(KeyCode.P))
+        {
+            Beep.volume = 1.0F;
+        }
+    }
 }
